69fd767ae8041fbbe5e16a18e4a414c23c1386c9
Isabelle Lopes <isabelle.biomed@gmail.com>
2025-04-20 21:51:49 -0300

    feat: alteração da tela inicio para não deixar em branco

--- src/app/core/enums/codigoTipoPropriedadeEnum.ts
+++
@@ -1,9 +0,0 @@
-export enum CodigoTipoPropriedadeEnum {
-    PROPRIO = "1",
-    TERCEIRO = "2"
-}
-
-export const CodigoPropriedadeDescricao = [
-    { value: CodigoTipoPropriedadeEnum.PROPRIO, label: 'Próprio' },
-    { value: CodigoTipoPropriedadeEnum.TERCEIRO, label: 'Terceiro' }
-];
