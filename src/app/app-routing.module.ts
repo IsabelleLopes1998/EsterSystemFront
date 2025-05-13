408af7347c252d4ed87055eeb8fa5f512a0f6d08
Ian Marcelino <iandevcontato@gmail.com>
2025-05-13 11:20:22 -0300

    Compra listar implementação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,57 +4,39 @@ import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component';
 import { ClienteListarComponent } from './pages/cliente/cliente-listar/cliente-listar.component';
 import { ClienteCriarNovoComponent } from './pages/cliente/cliente-criar-novo/cliente-criar-novo.component';
-
 import { ProdutoListarComponent } from './pages/produto/produto-listar/produto-listar.component';
 import { ProdutoCriarNovoComponent } from './pages/produto/produto-criar-novo/produto-criar-novo.component';
-
 import { CategoriaListarComponent } from './pages/categoria/categoria-listar/categoria-listar.component';
 import { CategoriaCriarNovoComponent } from './pages/categoria/categoria-criar-novo/categoria-criar-novo.component';
-
+import { EstoqueListarComponent } from './pages/estoque/estoque-listar/estoque-listar.component';
+import { CompraListarComponent } from './pages/compra/compra-listar/compra-listar.component';
 import { LoginComponent } from './pages/login/login.component';
 import { AuthGuard } from './auth.guard';
 
-import { EstoqueListarComponent } from './pages/estoque/estoque-listar/estoque-listar.component'
-
-
 const routes: Routes = [
-    // Redirecionamento automático para /login ao abrir a aplicação
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
-
-    {
-        path: '',
-        children: [
-            { path: 'index', component: IndexComponent },
-            //       {
-            //         path: 'index',
-            //         loadComponent: () => import('../app/index/index.component').then(m => m.IndexComponent),
-            //         canActivate: [AuthGuard]
-            //       },
-
-            { path: 'cliente-criar-novo', component: ClienteCriarNovoComponent },
-            { path: 'cliente-criar-novo/:id', component: ClienteCriarNovoComponent },
-            { path: 'cliente-listar', component: ClienteListarComponent },
-            { path: 'produto-listar', component: ProdutoListarComponent },
-            { path: 'produto-criar-novo/:id', component: ProdutoCriarNovoComponent },
-            { path: 'categoria-listar', component: CategoriaListarComponent },
-            { path: 'categoria-criar-novo', component: CategoriaCriarNovoComponent },
-            { path: 'produto-criar-novo', component: ProdutoCriarNovoComponent },
-            { path: 'categoria-criar-novo/:id', component: CategoriaCriarNovoComponent },
-            { path: 'estoque-listar', component: EstoqueListarComponent }
-            //       { path: 'financeiro', component: FinanceiroComponent, canActivate: [AuthGuard] },
-            //       { path: 'usuario-criar-novo', component: UsuarioCriarNovoComponent, canActivate: [AuthGuard] },
-            //       { path: 'usuario-criar-novo/:id', component: UsuarioCriarNovoComponent, canActivate: [AuthGuard] },
-            //       { path: 'usuario-listar', component: UsuarioListarComponent, canActivate: [AuthGuard] }
-        ]
-    },
-
-    // Caso a rota não exista, redireciona para login
-    { path: '**', redirectTo: 'index' }
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: '',
+    children: [
+      { path: 'index', component: IndexComponent },
+      { path: 'cliente-criar-novo', component: ClienteCriarNovoComponent },
+      { path: 'cliente-criar-novo/:id', component: ClienteCriarNovoComponent },
+      { path: 'cliente-listar', component: ClienteListarComponent },
+      { path: 'produto-listar', component: ProdutoListarComponent },
+      { path: 'produto-criar-novo', component: ProdutoCriarNovoComponent },
+      { path: 'produto-criar-novo/:id', component: ProdutoCriarNovoComponent },
+      { path: 'categoria-listar', component: CategoriaListarComponent },
+      { path: 'categoria-criar-novo', component: CategoriaCriarNovoComponent },
+      { path: 'categoria-criar-novo/:id', component: CategoriaCriarNovoComponent },
+      { path: 'estoque-listar', component: EstoqueListarComponent },
+      { path: 'compra-listar', component: CompraListarComponent }
+    ]
+  },
+  { path: '**', redirectTo: 'index' }
 ];
 
-
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
