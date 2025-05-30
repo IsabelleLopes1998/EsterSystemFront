1d15a936e6b8a6df897a602a7222e5237c2b34fc
Ian Marcelino <iandevcontato@gmail.com>
2025-05-29 21:10:58 -0300

    fix: correção de erro na criação de cliente

--- src/app/pages/cliente/cliente.service.ts
+++ src/app/pages/cliente/cliente.service.ts
@@ -9,16 +9,15 @@ import { ClienteResponse } from './cliente-listar/cliente.model';
 })
 export class ClienteService {
 
-  private clienteApiUrl = `${environment.apiBaseUrl}/clientes`;
-
+  private readonly clienteApiUrl = `${environment.apiBaseUrl}/clientes`;
 
   constructor(private http: HttpClient) { }
-
 
 
   salvarCliente(cliente: any): Observable<ClienteResponse> {
     return this.http.post<ClienteResponse>(`${this.clienteApiUrl}/salvarCliente`, cliente);
   }
+
 
   getListaDeClientes(): Observable<ClienteResponse[]> {
     return this.http.get<ClienteResponse[]>(`${this.clienteApiUrl}/listaDeClientes`);
@@ -29,32 +28,15 @@ export class ClienteService {
   }
 
   atualizarCliente(cliente: ClienteResponse): Observable<ClienteResponse> {
-    return this.http.put<ClienteResponse>(`${this.clienteApiUrl}/atualizarCliente/${cliente.id}`, cliente);
+    return this.http.put<ClienteResponse>(
+      `${this.clienteApiUrl}/atualizarCliente/${cliente.id}`,
+      cliente
+    );
   }
+
 
   excluirCliente(id: string): Observable<void> {
     return this.http.delete<void>(`${this.clienteApiUrl}/excluirCliente/${id}`);
   }
 
-  listarTodos(): Observable<any[]> {
-    return this.http.get<any[]>(this.clienteApiUrl);
-  }
-
-  buscarPorId(id: number): Observable<any> {
-    return this.http.get<any>(`${this.clienteApiUrl}/${id}`);
-  }
-
-  criar(cliente: any): Observable<any> {
-    return this.http.post(this.clienteApiUrl, cliente);
-  }
-
-  atualizar(id: number, cliente: any): Observable<any> {
-    return this.http.put(`${this.clienteApiUrl}/${id}`, cliente);
-  }
-
-  excluir(id: number): Observable<any> {
-    return this.http.delete(`${this.clienteApiUrl}/${id}`);
-  }
-
 }
-
